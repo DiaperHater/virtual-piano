7cb5c0c0d47bd05fe17ddb3d69e9ab9f089a26b9
314ronin <pownmymail@gmail.com>
2021-03-10 18:37:57 +0300

    implement style change of active piano key. Close #3

--- script.js
+++ script.js
@@ -4,6 +4,26 @@
 
 
 
+
+//Piano key active style change
+const piano = document.querySelector('.piano')
+const pianoKeys = document.querySelectorAll('.piano-key');
+const keyActive = 'piano-key-active';
+
+piano.addEventListener('click', (event) => {
+	if(event.target.classList.contains('piano-key')) {
+		pianoKeys.forEach(key => key.classList.remove(keyActive));
+		event.target.classList.add(keyActive);
+	}
+});
+
+
+
+
+
+
+
+
 // Switch buttons style change
 const switchToNotesBtn = document.querySelector('.btn-notes');
 const switchToLettersBtn = document.querySelector('.btn-letters');
